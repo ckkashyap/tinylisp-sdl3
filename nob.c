b712c8c862d573834869311d23eb0ed0e63309bb
kashyap <ckkashyap@gmail.com>
2025-11-16 22:05:07 -0800

    backtick

--- nob.c
+++ nob.c
@@ -27,29 +27,59 @@ static void append_flags_from_file(Nob_Cmd *cmd, char *flag_file)
     nob_sb_free(sb);
 }
 
-int main(int argc, char **argv) {
+const char *backtick(Nob_Cmd *cmd)
+{
+    char *libdir_tmp = nob_temp_sprintf("sdl_libdir.tmp");
+    if (!nob_cmd_run(cmd, .stdout_path = libdir_tmp))
+    {
+        nob_log(NOB_ERROR, "Failed to run %s", cmd->items[0]);
+        return NULL;
+    }
+
+    Nob_String_Builder libdir_sb = {0};
+    if (!nob_read_entire_file(libdir_tmp, &libdir_sb))
+    {
+        nob_log(NOB_ERROR, "Failed to read libdir from %s", libdir_tmp);
+        return NULL;
+    }
+
+    const char *sdl_libdir = nob_temp_sv_to_cstr(nob_sb_to_sv(libdir_sb));
+    nob_sb_free(libdir_sb);
+    nob_delete_file(libdir_tmp);
+
+
+    return sdl_libdir;
+}
+
+int main(int argc, char **argv)
+{
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    if (argc > 1 && nob_sv_eq(nob_sv_from_cstr(argv[1]), nob_sv_from_cstr("clean"))) {
+    if (argc > 1 && nob_sv_eq(nob_sv_from_cstr(argv[1]), nob_sv_from_cstr("clean")))
+    {
         Nob_Cmd clean_cmd = {0};
         nob_cmd_append(&clean_cmd, "rm", "-rf", "lisp-sdl3");
-        if (!nob_cmd_run(&clean_cmd)) {
+        if (!nob_cmd_run(&clean_cmd))
+	{
             return 1;
         }
         return 0;
     }
 
-    // Build
-    Nob_Cmd cmd = {0};
+    Nob_Cmd xx = {0};
+    nob_cmd_append(&xx, "pkg-config", "--libs", "sdl3");
+    printf("----------> %s <-----------\n",backtick(&xx));
 
+
+    // Build
     char* sdl_extra_cflags = "-ISDL_ttf/include";
-    char* readline_ldflags = "-lreadline";
 
     // Get SDL_CFLAGS from pkg-config
     Nob_Cmd pkg_cflags_cmd = {0};
     nob_cmd_append(&pkg_cflags_cmd, "pkg-config", "--cflags", "sdl3");
     char *cflags_tmp = nob_temp_sprintf("sdl_cflags.tmp");
-    if (!nob_cmd_run(&pkg_cflags_cmd, .stdout_path = cflags_tmp)) {
+    if (!nob_cmd_run(&pkg_cflags_cmd, .stdout_path = cflags_tmp))
+    {
         nob_log(NOB_ERROR, "Failed to run pkg-config --cflags sdl3");
         return 1;
     }
@@ -58,22 +88,25 @@ int main(int argc, char **argv) {
     Nob_Cmd pkg_libdir_cmd = {0};
     nob_cmd_append(&pkg_libdir_cmd, "pkg-config", "--variable=libdir", "sdl3");
     char *libdir_tmp = nob_temp_sprintf("sdl_libdir.tmp");
-    if (!nob_cmd_run(&pkg_libdir_cmd, .stdout_path = libdir_tmp)) {
+    if (!nob_cmd_run(&pkg_libdir_cmd, .stdout_path = libdir_tmp))
+    {
         nob_log(NOB_ERROR, "Failed to run pkg-config --variable=libdir sdl3");
         return 1;
     }
+
     Nob_String_Builder libdir_sb = {0};
-    if (!nob_read_entire_file(libdir_tmp, &libdir_sb)) {
+    if (!nob_read_entire_file(libdir_tmp, &libdir_sb))
+    {
         nob_log(NOB_ERROR, "Failed to read libdir from %s", libdir_tmp);
         return 1;
     }
+
     const char *sdl_libdir = nob_temp_sv_to_cstr(nob_sb_to_sv(libdir_sb));
     nob_sb_free(libdir_sb);
     nob_delete_file(libdir_tmp);
 
-    const char* sdl_ldflags = nob_temp_sprintf("-L%s -lSDL3 -LSDL_ttf/build -lSDL3_ttf -Wl,-rpath,SDL_ttf/build", sdl_libdir);
 
-    // Common CFLAGS: -g + readline_cflags + sdl_cflags (already appended for Linux)
+    Nob_Cmd cmd = {0};
     nob_cmd_append(&cmd, "gcc", "-g");
     append_flags_from_file(&cmd, cflags_tmp);
     nob_cmd_append(&cmd, sdl_extra_cflags);
@@ -84,34 +117,42 @@ int main(int argc, char **argv) {
     // Output
     nob_cmd_append(&cmd, "-o", "lisp-sdl3");
 
-    // LDFLAGS: -lm + readline_ldflags + sdl_ldflags
     nob_cmd_append(&cmd, "-lm");
+
+    char* readline_ldflags = "-lreadline";
     if (strlen(readline_ldflags) > 0) {
         // Split and append if multi-arg
         Nob_String_View rl_ld_sv = nob_sv_from_cstr(readline_ldflags);
-        while (rl_ld_sv.count > 0) {
+        while (rl_ld_sv.count > 0)
+	{
             Nob_String_View flag = nob_sv_chop_by_delim(&rl_ld_sv, ' ');
             flag = nob_sv_trim(flag);
-            if (flag.count > 0) {
-                const char *flag_str = nob_temp_sv_to_cstr(flag);
-                nob_cmd_append(&cmd, flag_str);
-            }
-        }
-    }
-    // sdl_ldflags may be multi-arg
-    if (strlen(sdl_ldflags) > 0) {
-        Nob_String_View sdl_ld_sv = nob_sv_from_cstr(sdl_ldflags);
-        while (sdl_ld_sv.count > 0) {
-            Nob_String_View flag = nob_sv_chop_by_delim(&sdl_ld_sv, ' ');
-            flag = nob_sv_trim(flag);
-            if (flag.count > 0) {
+            if (flag.count > 0)
+	    {
                 const char *flag_str = nob_temp_sv_to_cstr(flag);
                 nob_cmd_append(&cmd, flag_str);
             }
         }
     }
 
-    if (!nob_cmd_run(&cmd)) {
+    const char* sdl_ldflags = nob_temp_sprintf("-L%s -lSDL3 -LSDL_ttf/build -lSDL3_ttf -Wl,-rpath,SDL_ttf/build", sdl_libdir);
+    if (strlen(sdl_ldflags) > 0)
+    {
+        Nob_String_View sdl_ld_sv = nob_sv_from_cstr(sdl_ldflags);
+        while (sdl_ld_sv.count > 0)
+	{
+            Nob_String_View flag = nob_sv_chop_by_delim(&sdl_ld_sv, ' ');
+            flag = nob_sv_trim(flag);
+            if (flag.count > 0)
+	    {
+                const char *flag_str = nob_temp_sv_to_cstr(flag);
+                nob_cmd_append(&cmd, flag_str);
+            }
+        }
+    }
+
+    if (!nob_cmd_run(&cmd))
+    {
         return 1;
     }
 
